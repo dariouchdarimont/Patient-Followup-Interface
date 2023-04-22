e37c879055b1eaeb5c3385da977587c00a98061c
Dariouch <56547417+dariouchdarimont@users.noreply.github.com>
2023-04-22 23:17:26 +0200

    project init

---
+++ Patient-Followup-Project/src/main/java/com/mycompany/patient/followup/project/PatientFollowupProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.patient.followup.project;
+
+/**
+ *
+ * @author dardar2000
+ */
+public class PatientFollowupProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
