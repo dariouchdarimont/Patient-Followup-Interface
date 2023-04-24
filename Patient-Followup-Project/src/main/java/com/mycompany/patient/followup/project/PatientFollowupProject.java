d20545acbb84f875244b605d883d1e1b6ade1d52
sarozasza <sarah-djihad.rozas.zabalo@ulb.be>
2023-04-24 17:17:22 +0200

    test

--- Patient-Followup-Project/src/main/java/com/mycompany/patient/followup/project/PatientFollowupProject.java
+++ Patient-Followup-Project/src/main/java/com/mycompany/patient/followup/project/PatientFollowupProject.java
@@ -11,6 +11,6 @@ package com.mycompany.patient.followup.project;
 public class PatientFollowupProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("slt bg");
     }
 }
